39ae79b837091c530552d032b1bbe356d1dffa14
ProfessorVishal <107393840+CallMeProfessor127001@users.noreply.github.com>
2024-02-15 12:58:52 +0000

    Add files via upload

---
+++ ARCHrequirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+sudo pacman -Syy
+sudo pacman -Syu
+sudo pacman -S figlet
+sudo pacman -S cowsay
+sudo pacman -S lolcat
+sudo pacman -S toilet
+
+sudo pacman -S curl
+sudo pacman -S wget
---
+++ DEBrequirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
+sudo apt-get clean
+
+sudo apt-get install figlet
+sudo apt-get install cowsay
+sudo apt-get install lolcat
+sudo apt-get install toilet
+
+sudo apt-get install wget
+sudo apt-get install curl
